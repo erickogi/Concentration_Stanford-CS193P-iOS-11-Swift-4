e205a6e7dbd16df3b654bd7c08af8ed74688332d
erickogi <erickogi14@gmail.com>
2020-03-25 15:36:36 +0300

    Initial Commit

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func touchPumpkinCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
     }
-
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle==emoji{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+            
+        }
+       
+    }
+    
 
 }
 
